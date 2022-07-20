fb3938bb354ff4784bc10a8aaf3a42a3e1b93d5c
vam <dm@vamist.dev>
2022-07-20 22:05:18 +0100

    [dev] Init

---
+++ Server/servertest_spawnregions.lua
@@ -0,0 +1,29 @@
+function SpawnRegions()
+
+	return {
+
+		{ name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+
+		{ name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
+
+		{ name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
+
+		{ name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
+
+		{ name = "STEVVPLSpawn", file = "media/maps/STEVVPLSpawn/spawnpoints.lua" },
+
+		{ name = "ModTemplateWorld1", file = "media/maps/ModTemplateWorld1/spawnpoints.lua" },
+
+		{ name = "ModTemplateWorld2", file = "media/maps/ModTemplateWorld2/spawnpoints.lua" },
+
+		{ name = "Deerhead Lake, KY", file = "media/maps/Deerhead Lake, KY/spawnpoints.lua" },
+
+		{ name = "March Ridge, KY", file = "media/maps/March Ridge, KY/spawnpoints.lua" },
+
+		{ name = "STEVSpawn", file = "media/maps/STEVSpawn/spawnpoints.lua" },
+
+		{ name = "STRSpawn", file = "media/maps/STRSpawn/spawnpoints.lua" },
+
+	}
+
+end
