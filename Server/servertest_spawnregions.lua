88a1184c3ba20e6c29babd7173f223ca13d1e007
Vamist <personal@vamist.dev>
2022-07-30 17:44:27 +0100

    [dev] Removed spawnpoints

--- Server/servertest_spawnregions.lua
+++ Server/servertest_spawnregions.lua
@@ -2,14 +2,6 @@ function SpawnRegions()
 
 	return {
 
-		{ name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
-
-		{ name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
-
-		{ name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
-
-		{ name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
-
 		{ name = "STEVVPLSpawn", file = "media/maps/STEVVPLSpawn/spawnpoints.lua" },
 
 		{ name = "ModTemplateWorld1", file = "media/maps/ModTemplateWorld1/spawnpoints.lua" },
