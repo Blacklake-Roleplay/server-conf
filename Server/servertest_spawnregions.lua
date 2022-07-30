e58bd4feaac410e6ee54255c546660ea3fd8523e
Vamist <personal@vamist.dev>
2022-07-30 17:31:06 +0100

    [test] set spawnpoints again

--- Server/servertest_spawnregions.lua
+++ Server/servertest_spawnregions.lua
@@ -20,6 +20,10 @@ function SpawnRegions()
 
 		{ name = "March Ridge, KY", file = "media/maps/March Ridge, KY/spawnpoints.lua" },
 
+		{ name = "Ekron, KY", file = "media/maps/Ekron, KY/spawnpoints.lua" },
+
+		{ name - "Preston Army Depot, KY", file = "media/maps/Preston Army Depot, KY/spawnpoints.lua" },
+
 		{ name = "STEVSpawn", file = "media/maps/STEVSpawn/spawnpoints.lua" },
 
 		{ name = "STRSpawn", file = "media/maps/STRSpawn/spawnpoints.lua" },
