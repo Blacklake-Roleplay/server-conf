6c82c92fbd1e4f87ff8ec31976f60ad04b663483
Divisionic <120514170+Divisionic@users.noreply.github.com>
2022-12-28 00:51:33 +0100

    Adds Second Random Spawnpoint

--- Server/servertest_spawnregions.lua
+++ Server/servertest_spawnregions.lua
@@ -1,6 +1,7 @@
 function SpawnRegions()
     return {
         { name = "Clear Cove", file = "media/maps/clearcove/spawnpoints.lua" },
-        { name = "Random Spawn", serverfile = "servertest_spawnpoint.lua" },
+        { name = "Random Spawn", serverfile = "server/servertest_spawnpoint.lua" },
+        { name = "Random Spawn 2", serverfile = "server/NorthRoad_spawnpoint.lua"},
     }
 end
