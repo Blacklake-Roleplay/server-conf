906dd5d5652cc0d4e9818f115730f0c9b7420ef3
Divisionic <120514170+Divisionic@users.noreply.github.com>
2022-12-28 00:23:49 +0100

    Adds a new script for randomized spawning

--- Server/servertest_spawnregions.lua
+++ Server/servertest_spawnregions.lua
@@ -1,5 +1,6 @@
 function SpawnRegions()
     return {
         { name = "Clear Cove", file = "media/maps/clearcove/spawnpoints.lua" },
+        { name = "Random Spawn", serverfile = "NorthRoad_spawnpoint.lua" }
     }
 end
