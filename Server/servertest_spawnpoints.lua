22a32a0f9566bd3cc62f66ae99c15d9f8f50ce1b
Divisionic <120514170+Divisionic@users.noreply.github.com>
2022-12-28 00:07:51 +0100

    changes "Unemployed" spawn values

--- Server/servertest_spawnpoints.lua
+++ Server/servertest_spawnpoints.lua
@@ -34,10 +34,10 @@ function SpawnPoints()
 		{ worldX = 6, worldY = 2, posX = 268, posY = 110, posZ = 0 }
 	  },
 	  unemployed = {
-		{ worldX = 6, worldY = 1, posX = 51, posY = 212, posZ = 0 },
-		{ worldX = 6, worldY = 2, posX = 239, posY = 64, posZ = 0 },
-		{ worldX = 6, worldY = 2, posX = 68, posY = 49, posZ = 0 },
-		{ worldX = 6, worldY = 2, posX = 268, posY = 110, posZ = 0 }
+		{ worldX = 1, worldY = 2, posX = 11, posY = 162, posZ = 0 },
+		{ worldX = 0, worldY = 7, posX = 41, posY = 157, posZ = 0 },
+		{ worldX = 5, worldY = 27, posX = 120, posY = 239, posZ = 0 },
+		{ worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
 	  }
 	}
 	end
