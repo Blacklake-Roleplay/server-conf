9692513adf2e366672dc8132373bfcbc8ffe6709
Divisionic <120514170+Divisionic@users.noreply.github.com>
2022-12-27 20:40:16 +0100

    Adds NorthRoad_spawnpoint.lua file
    didn't want to change the existing files, so I made my own one quickly

---
+++ Server/NorthRoad_spawnpoint.lua
@@ -0,0 +1,55 @@
+function Spawnpoints()
+        return {
+            unemployed = {
+                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+            },
+            fireofficer = {
+                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+            },
+            policeofficer = {
+                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+            },
+            parkranger = {
+                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+            },
+            securityguard = {
+                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+            },
+            carpenter = {
+                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+            },
+            burglar = {
+                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+            },
+            chef = {
+                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+            },
+            repairman = {
+                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+            },
+            farmer = {
+                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+            },
+            fisherman = {
+                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+            },
+            doctor = {
+                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+            },
+            veteran = {
+                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+            },
+            nurse = {
+                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+            },
+            engineer = {
+                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+            },
+            smither = {
+                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+            },
+            mechanics = {
+                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+            }
+        }
+        end
