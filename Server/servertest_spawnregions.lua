1abec769d23552db24302b1de11a9ef942469a40
Divisionic <120514170+Divisionic@users.noreply.github.com>
2022-12-27 20:42:40 +0100

    Adds New Spawnlocation
    Testing to see if my file works

--- Server/servertest_spawnregions.lua
+++ Server/servertest_spawnregions.lua
@@ -1,5 +1,6 @@
 function SpawnRegions()
     return {
         { name = "Clear Cove", file = "media/maps/clearcove/spawnpoints.lua" },
+        { name = "North Road", serverfile = "NorthRoad_spawnpoint.lua" },
     }
 end
