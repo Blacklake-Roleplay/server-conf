d0b959498a8a7b10c863b10de9adef29af218fd6
Divisionic <120514170+Divisionic@users.noreply.github.com>
2022-12-28 00:35:01 +0100

    Fixes to correct lua file

--- Server/servertest_spawnregions.lua
+++ Server/servertest_spawnregions.lua
@@ -1,6 +1,6 @@
 function SpawnRegions()
     return {
         { name = "Clear Cove", file = "media/maps/clearcove/spawnpoints.lua" },
-        { name = "Random Spawn", serverfile = "NorthRoad_spawnpoint.lua" }
+        { name = "Random Spawn", serverfile = "servertest_spawnpoint.lua" }
     }
 end
