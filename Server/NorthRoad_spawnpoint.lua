fdd1de1e98b3b41f3fcba546d65bd991aed78d1c
Divisionic <120514170+Divisionic@users.noreply.github.com>
2022-12-28 01:18:16 +0100

    Adds Spawn Options lua files

--- Server/NorthRoad_spawnpoint.lua
+++ Server/NorthRoad_spawnpoint.lua
@@ -1,55 +1,55 @@
 function Spawnpoints()
         return {
             unemployed = {
-                { worldX = 1, worldY = 3, posX = 311, posY = 162, posZ = 0 }
+                { worldX = 1, worldY = 2, posX = 11, posY = 162, posZ = 0 }
             },
             fireofficer = {
-                { worldX = 1, worldY = 3, posX = 311, posY = 162, posZ = 0 }
+                { worldX = 1, worldY = 2, posX = 11, posY = 162, posZ = 0 }
             },
             policeofficer = {
-                { worldX = 1, worldY = 3, posX = 311, posY = 162, posZ = 0 }
+                { worldX = 1, worldY = 2, posX = 11, posY = 162, posZ = 0 }
             },
             parkranger = {
-                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+                { worldX = 1, worldY = 2, posX = 11, posY = 162, posZ = 0 }
             },
             securityguard = {
-                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+                { worldX = 1, worldY = 2, posX = 11, posY = 162, posZ = 0 }
             },
             carpenter = {
-                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+                { worldX = 1, worldY = 2, posX = 11, posY = 162, posZ = 0 }
             },
             burglar = {
-                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+                { worldX = 1, worldY = 2, posX = 11, posY = 162, posZ = 0 }
             },
             chef = {
-                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+                { worldX = 1, worldY = 2, posX = 11, posY = 162, posZ = 0 }
             },
             repairman = {
-                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+                { worldX = 1, worldY = 2, posX = 11, posY = 162, posZ = 0 }
             },
             farmer = {
-                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+                { worldX = 1, worldY = 2, posX = 11, posY = 162, posZ = 0 }
             },
             fisherman = {
-                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+                { worldX = 1, worldY = 2, posX = 11, posY = 162, posZ = 0 }
             },
             doctor = {
-                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+                { worldX = 1, worldY = 2, posX = 11, posY = 162, posZ = 0 }
             },
             veteran = {
-                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+                { worldX = 1, worldY = 2, posX = 11, posY = 162, posZ = 0 }
             },
             nurse = {
-                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+                { worldX = 1, worldY = 2, posX = 11, posY = 162, posZ = 0 }
             },
             engineer = {
-                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+                { worldX = 1, worldY = 2, posX = 11, posY = 162, posZ = 0 }
             },
             smither = {
-                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+                { worldX = 1, worldY = 2, posX = 11, posY = 162, posZ = 0 }
             },
             mechanics = {
-                { worldX = 311, worldY = 762, posX = 311, posY = 762, posZ = 0 }
+                { worldX = 1, worldY = 2, posX = 11, posY = 162, posZ = 0 }
             }
         }
         end
