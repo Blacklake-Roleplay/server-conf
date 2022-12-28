c6bc7bc4c97d5d0ce33db57f81b416a2bcc722ac
Divisionic <120514170+Divisionic@users.noreply.github.com>
2022-12-28 01:44:11 +0100

    Fixes case and tabs

--- Server/EastRoad_spawnpoint.lua
+++ Server/EastRoad_spawnpoint.lua
@@ -1,55 +1,55 @@
-function Spawnpoints()
-    return {
-        unemployed = {
-            { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
-        },
-        fireofficer = {
-            { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
-        },
-        policeofficer = {
-            { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
-        },
-        parkranger = {
-            { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
-        },
-        securityguard = {
-            { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
-        },
-        carpenter = {
-            { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
-        },
-        burglar = {
-            { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
-        },
-        chef = {
-            { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
-        },
-        repairman = {
-            { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
-        },
-        farmer = {
-            { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
-        },
-        fisherman = {
-            { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
-        },
-        doctor = {
-            { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
-        },
-        veteran = {
-            { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
-        },
-        nurse = {
-            { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
-        },
-        engineer = {
-            { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
-        },
-        smither = {
-            { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
-        },
-        mechanics = {
-            { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
-        }
+function SpawnPoints()
+return {
+    unemployed = {
+        { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
+    },
+    fireofficer = {
+        { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
+    },
+    policeofficer = {
+        { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
+    },
+    parkranger = {
+        { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
+    },
+    securityguard = {
+        { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
+    },
+    carpenter = {
+        { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
+    },
+    burglar = {
+        { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
+    },
+    chef = {
+        { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
+    },
+    repairman = {
+        { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
+    },
+    farmer = {
+        { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
+    },
+    fisherman = {
+        { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
+    },
+    doctor = {
+        { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
+    },
+    veteran = {
+        { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
+    },
+    nurse = {
+        { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
+    },
+    engineer = {
+        { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
+    },
+    smither = {
+        { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
+    },
+    mechanics = {
+        { worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
     }
-    end
+}
+end
