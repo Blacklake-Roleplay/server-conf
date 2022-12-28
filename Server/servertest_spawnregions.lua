4254c636ebabe25bbda561dd0a6c219967b093bf
Divisionic <120514170+Divisionic@users.noreply.github.com>
2022-12-28 02:15:48 +0100

    Removes "Clear Cove" as Spawn Option [IMPORTANT] [FOR DEFAULT SPAWN RE-ADD]

--- Server/servertest_spawnregions.lua
+++ Server/servertest_spawnregions.lua
@@ -1,6 +1,5 @@
 function SpawnRegions()
     return {
-        { name = "Clear Cove", file = "media/maps/clearcove/spawnpoints.lua" },
         { name = "North Road", serverfile = "NorthRoad_spawnpoint.lua" },
         { name = "East Road", serverfile = "EastRoad_spawnpoint.lua"},
         { name = "South Road", serverfile = "SouthRoad_spawnpoint.lua"},
