a762f1571149e5fd09405f71113bca4bd995bb66
Divisionic <120514170+Divisionic@users.noreply.github.com>
2022-12-28 01:20:02 +0100

    Adds Spawnoptions in Spawnregions

--- Server/servertest_spawnregions.lua
+++ Server/servertest_spawnregions.lua
@@ -1,7 +1,9 @@
 function SpawnRegions()
     return {
         { name = "Clear Cove", file = "media/maps/clearcove/spawnpoints.lua" },
-        { name = "Random Spawn", serverfile = "server/servertest_spawnpoint.lua" },
-        { name = "Random Spawn 2", serverfile = "server/NorthRoad_spawnpoint.lua"},
+        { name = "North Road", serverfile = "server/NorthRoad_spawnpoint.lua" },
+        { name = "East Road", serverfile = "server/EastRoad_spawnpoint.lua"},
+        { name = "South Road", serverfile = "server/SouthRoad_spawnpoint.lua"},
+        { name = "West Road", serverfile = "server/WestRoad_spawnpoint.lua"},
     }
 end
