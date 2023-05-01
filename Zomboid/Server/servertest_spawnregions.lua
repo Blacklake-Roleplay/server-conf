a62e6ceebd655291e2dfc20a4d1470521c560c32
Vamist <personal@vamist.dev>
2023-05-01 13:01:39 +0100

    [rm] spawn points again

--- Zomboid/Server/servertest_spawnregions.lua
+++
@@ -1,8 +0,0 @@
-function SpawnRegions()
-    return {
-        { name = "North Road", serverfile = "NorthRoad_spawnpoint.lua" },
-        { name = "East Road", serverfile = "EastRoad_spawnpoint.lua"},
-        { name = "South Road", serverfile = "SouthRoad_spawnpoint.lua"},
-        { name = "West Road", serverfile = "WestRoad_spawnpoint.lua"}
-    }
-end
