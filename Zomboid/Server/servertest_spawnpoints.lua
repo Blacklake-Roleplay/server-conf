148c3fb284d76ea589b2453f8ac06294ee18299f
Oreo <oreo@vamist.dev>
2023-05-01 12:00:43 +0000

    [SERVER] Automated push

---
+++ Zomboid/Server/servertest_spawnpoints.lua
@@ -0,0 +1,43 @@
+function SpawnPoints()
+	return {
+	  chef = {
+		{ worldX = 6, worldY = 2, posX = 68, posY = 49, posZ = 0 },
+		{ worldX = 6, worldY = 2, posX = 89, posY = 179, posZ = 0 }
+	  },
+	  constructionworker = {
+		{ worldX = 6, worldY = 1, posX = 51, posY = 212, posZ = 0 },
+		{ worldX = 6, worldY = 2, posX = 239, posY = 64, posZ = 0 },
+		{ worldX = 6, worldY = 2, posX = 68, posY = 49, posZ = 0 },
+		{ worldX = 6, worldY = 2, posX = 268, posY = 110, posZ = 0 }
+	  },
+	  doctor = {
+		{ worldX = 6, worldY = 1, posX = 51, posY = 212, posZ = 0 },
+		{ worldX = 6, worldY = 2, posX = 127, posY = 129, posZ = 0 },
+		{ worldX = 6, worldY = 2, posX = 268, posY = 110, posZ = 0 }
+	  },
+	  fireofficer = {
+		{ worldX = 6, worldY = 2, posX = 239, posY = 64, posZ = 0 },
+		{ worldX = 6, worldY = 2, posX = 95, posY = 130, posZ = 0 }
+	  },
+	  parkranger = {
+		{ worldX = 6, worldY = 1, posX = 143, posY = 233, posZ = 0 },
+		{ worldX = 6, worldY = 2, posX = 239, posY = 64, posZ = 0 }
+	  },
+	  policeofficer = {
+		{ worldX = 6, worldY = 1, posX = 51, posY = 212, posZ = 0 },
+		{ worldX = 6, worldY = 2, posX = 68, posY = 49, posZ = 0 },
+		{ worldX = 6, worldY = 2, posX = 147, posY = 144, posZ = 0 }
+	  },
+	  securityguard = {
+		{ worldX = 6, worldY = 2, posX = 239, posY = 64, posZ = 0 },
+		{ worldX = 6, worldY = 2, posX = 68, posY = 49, posZ = 0 },
+		{ worldX = 6, worldY = 2, posX = 268, posY = 110, posZ = 0 }
+	  },
+	  unemployed = {
+		{ worldX = 1, worldY = 2, posX = 11, posY = 162, posZ = 0 },
+		{ worldX = 0, worldY = 7, posX = 41, posY = 157, posZ = 0 },
+		{ worldX = 5, worldY = 27, posX = 120, posY = 239, posZ = 0 },
+		{ worldX = 9, worldY = 6, posX = 226, posY = 131, posZ = 0 }
+	  }
+	}
+	end
---
+++ Zomboid/Server/servertest_spawnregions.lua
@@ -0,0 +1,8 @@
+function SpawnRegions()
+    return {
+        { name = "North Road", serverfile = "NorthRoad_spawnpoint.lua" },
+        { name = "East Road", serverfile = "EastRoad_spawnpoint.lua"},
+        { name = "South Road", serverfile = "SouthRoad_spawnpoint.lua"},
+        { name = "West Road", serverfile = "WestRoad_spawnpoint.lua"}
+    }
+end
