06d76d296af0e39ffe91d595444b96e601f357ef
Totally_Innocent <51790682+Peter-David-Miller@users.noreply.github.com>
2024-02-17 17:16:52 +0000

    Remove Spawn Clinic

--- Server/servertest_spawnregions.lua
+++ Server/servertest_spawnregions.lua
@@ -2,6 +2,5 @@ function SpawnRegions()
 	return {
 		{ name = "Romero", file = "media/maps/Romero/spawnpoints.lua" },
 		{ name = "Conquest", file = "media/maps/Conquest/spawnpoints.lua" },
-		{ name = "Romero Clinic", file = "media/maps/RomeroClinic/spawnpoints.lua" },
 	}
 end
