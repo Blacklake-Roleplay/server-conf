6ffe022dfe23be293f203ad861fad0006dc3dcfe
Totally_Innocent <51790682+Peter-David-Miller@users.noreply.github.com>
2024-02-17 15:32:19 +0000

    Update servertest_spawnregions.lua

--- Server/servertest_spawnregions.lua
+++ Server/servertest_spawnregions.lua
@@ -1,7 +1,7 @@
 function SpawnRegions()
 	return {
 		{ name = "Romero", file = "media/maps/Romero/spawnpoints.lua" },
-		{ name = Conquest", file = "media/maps/Conquest/spawnpoints.lua" },
+		{ name = "Conquest", file = "media/maps/Conquest/spawnpoints.lua" },
 		{ name = "Romero Clinic", file = "media/maps/RomeroClinic/spawnpoints.lua" },
 	}
 end
