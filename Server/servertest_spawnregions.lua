b96b772ecfc17005522a9104e2f8a47b2de91845
Totally_Innocent <51790682+Peter-David-Miller@users.noreply.github.com>
2024-03-21 16:34:52 +0000

    Added Off Romero Spawn
    Added Off Romero Spawn

--- Server/servertest_spawnregions.lua
+++ Server/servertest_spawnregions.lua
@@ -2,5 +2,7 @@ function SpawnRegions()
 	return {
 		{ name = "Romero", file = "media/maps/Romero/spawnpoints.lua" },
 		{ name = "Conquest", file = "media/maps/Conquest/spawnpoints.lua" },
+		{ name = "Off The Romero", serverfile = "servertest_spawnpoints.lua" },
+
 	}
 end
